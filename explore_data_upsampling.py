1ecce0c3b86b3a538b20d693bdabdefd46592baa
Varun Anand <varun.anand2017@vitalum.ac.in>
2022-03-10 20:51:51 +0000

    visualisations

--- explore_data_upsampling.py
+++ explore_data_upsampling.py
@@ -1,14 +1,23 @@
 # import the necessary packages
-from uscnn.utils import Icosphere
+from matplotlib.cbook import normalize_kwargs
+from uscnn.layers import MeshConvTest, MeshConv
+from uscnn.utils import Icosphere, spmatmul, sparse2tensor
 from tqdm import tqdm
 import meshplot as mp
 import numpy as np
+import torch
 import glob
+import igl
 import os
 
 mp.offline()
-
 np.random.seed(7)
+
+# grab the path to the dataset files
+paths = list(glob.glob("/home/varun/datasets/2d3ds_sphere/area_1/*.npz"))
+
+# sort the paths according to the split
+paths = sorted(paths, key=lambda x: int(x.split(os.path.sep)[-1].split(".")[0].split("_")[-1]))
 
 
 def normalize(vectors, radius=1):
@@ -23,40 +32,173 @@ def normalize(vectors, radius=1):
     return vectors + unit * offset.reshape((-1, 1))
 
 
-if __name__ == "__main__":
-    # grab the path to the dataset files
-    paths = list(glob.glob(
-        r"C:\Users\spike\Documents\Python Scripts\MLP_CW4\ugscnn-master\ugscnn-master\new\ugscnn\experiments\exp3_2d3ds\2d3ds_sphere\area_1\*.npz"))
+def normalize_features(x):
+    return (x - x.min()) / (x.max() - x.min())
 
-    # sort the paths according to the split
-    paths = sorted(paths, key=lambda x: int(x.split(os.path.sep)[-1].split(".")[0].split("_")[-1]))
 
-    # constants
-    level = 6
+def upsamp(img, level):
     nv_pad = 30 * (4**level)
+    n_channels = 3
 
     # initialise an icosphere
     ico = Icosphere(level=level)
     ico_up = Icosphere(level=level + 1)
 
+    img = img[:, :n_channels]
+
+    img = img[:ico.vertices.shape[0]]
+
+    zeros_pad = np.zeros((nv_pad, n_channels))
+    img = np.concatenate((img, zeros_pad))
+
+    # # bilinear interpolation upsampling
+    # img[ico_up.faces[3:][::4, 0]] = (img[ico.faces[:, 0]] + img[ico.faces[:, 1]]) / 2
+    # img[ico_up.faces[3:][::4, 1]] = (img[ico.faces[:, 1]] + img[ico.faces[:, 2]]) / 2
+    # img[ico_up.faces[3:][::4, 2]] = (img[ico.faces[:, 2]] + img[ico.faces[:, 1]]) / 2
+
+    # reshape
+    img_ = img.reshape((1, 3, ico_up.nv))
+    img_ = torch.FloatTensor(img_)
+
+    out = np.squeeze(MeshConvTest(3, 1, mesh_lvl=(level + 1))(img_).detach().numpy())
+    out = out.reshape((4, ico_up.nv, 3))
+
+    G = igl.grad(ico_up.vertices, ico_up.faces)
+    grad_face = G.dot(img).reshape((*ico_up.faces.shape, 3), order="F")
+
+    # face gradients along cardinal directions
+    grad_face_ew = np.sum(np.multiply(grad_face, ico_up.EW[..., None]), keepdims=False, axis=-1).T
+    grad_face_ns = np.sum(np.multiply(grad_face, ico_up.NS[..., None]), keepdims=False, axis=-1).T
+
+    grad_face_ew = torch.FloatTensor(grad_face_ew[None, ...])
+    grad_face_ns = torch.FloatTensor(grad_face_ns[None, ...])
+
+    F2V = sparse2tensor(ico_up.F2V.tocoo())
+
+    # vertex gradients (weighted by face area)
+    grad_vert_ew = spmatmul(grad_face_ew, F2V).numpy().squeeze().T
+    grad_vert_ns = spmatmul(grad_face_ns, F2V).numpy().squeeze().T
+
+    # # plot the image and save it to disk
+    # output_img_path = f"figs/op/bl_identity.html"
+    # mp.plot(ico_up.vertices, ico_up.faces, c=out[0, :ico_up.vertices.shape[0]],
+    #         shading={"point_size": 0.5}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_laplacian.html"
+    # mp.plot(ico_up.vertices, ico_up.faces, c=np.mean(out[1, :ico_up.vertices.shape[0]], axis=-1),
+    #         shading={"point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_ew.html"
+    # mp.plot(ico_up.vertices, ico_up.faces, c=np.mean(grad_vert_ew, axis=-1), shading={
+    #         "point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_ns.html"
+    # mp.plot(ico_up.vertices, ico_up.faces, c=np.mean(grad_vert_ns, axis=-1), shading={
+    #         "point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    output_img_path = f"figs/op/bl_out_{level + 1}.html"
+
+    identity = normalize_features(out[0])
+    laplacian = normalize_features(out[1])
+    grad_vert_ew = normalize_features(grad_vert_ew)
+    grad_vert_ns = normalize_features(grad_vert_ns)
+
+    t = identity + laplacian + grad_vert_ew + grad_vert_ns
+    t = normalize_features(t)
+
+    mp.plot(ico_up.vertices, ico_up.faces, c=t, shading={"point_size": 0.5}, filename=output_img_path)
+
+    # return out[0] + out[1] + grad_vert_ew + grad_vert_ns
+    return t
+
+
+def downsamp(img, level):
+    nv_pad = 30 * (4**level)
+    n_channels = 3
+
+    # initialise an icosphere
+    ico = Icosphere(level=level)
+    ico_down = Icosphere(level=level - 1)
+
+    img = img[:, :n_channels]
+
+    # downsample
+    img = img[:ico_down.vertices.shape[0]]
+
+    # reshape
+    img_ = img.reshape((1, 3, ico_down.nv))
+    img_ = torch.FloatTensor(img_)
+
+    out = np.squeeze(MeshConvTest(3, 1, mesh_lvl=(level - 1))(img_).detach().numpy())
+    out = out.reshape((4, ico_down.nv, 3))
+
+    G = igl.grad(ico_down.vertices, ico_down.faces)
+    grad_face = G.dot(img).reshape((*ico_down.faces.shape, 3), order="F")
+
+    # face gradients along cardinal directions
+    grad_face_ew = np.sum(np.multiply(grad_face, ico_down.EW[..., None]), keepdims=False, axis=-1).T
+    grad_face_ns = np.sum(np.multiply(grad_face, ico_down.NS[..., None]), keepdims=False, axis=-1).T
+
+    grad_face_ew = torch.FloatTensor(grad_face_ew[None, ...])
+    grad_face_ns = torch.FloatTensor(grad_face_ns[None, ...])
+
+    F2V = sparse2tensor(ico_down.F2V.tocoo())
+
+    # vertex gradients (weighted by face area)
+    grad_vert_ew = spmatmul(grad_face_ew, F2V).numpy().squeeze().T
+    grad_vert_ns = spmatmul(grad_face_ns, F2V).numpy().squeeze().T
+
+    # plot the image and save it to disk
+    # output_img_path = f"figs/op/bl_identity.html"
+    # mp.plot(ico_down.vertices, ico_down.faces, c=out[0, :ico_down.vertices.shape[0]],
+    #         shading={"point_size": 0.5}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_laplacian.html"
+    # mp.plot(ico_down.vertices, ico_down.faces, c=np.mean(out[1, :ico_down.vertices.shape[0]], axis=-1),
+    #         shading={"point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_ew.html"
+    # mp.plot(ico_down.vertices, ico_down.faces, c=np.mean(grad_vert_ew, axis=-1), shading={
+    #         "point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    # output_img_path = f"figs/op/bl_ns.html"
+    # mp.plot(ico_down.vertices, ico_down.faces, c=np.mean(grad_vert_ns, axis=-1), shading={
+    #         "point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
+
+    output_img_path = f"figs/op/down_out_{level - 1}.html"
+
+    identity = normalize_features(out[0])
+    laplacian = normalize_features(out[1])
+    grad_vert_ew = normalize_features(grad_vert_ew)
+    grad_vert_ns = normalize_features(grad_vert_ns)
+
+    t = identity + laplacian + grad_vert_ew + grad_vert_ns
+    t = normalize_features(t)
+
+    mp.plot(ico_down.vertices, ico_down.faces, c=t, shading={"point_size": 0.5}, filename=output_img_path)
+
+    # return out[0] + out[1] + grad_vert_ew + grad_vert_ns
+    return t
+
+
+if __name__ == "__main__":
     # load a sample image
     arr = np.load(paths[0])
     img, lbl = arr["data"], arr["labels"]
 
+    level = 4
+    img_up = img
+    for lvl in range(3, level):
+        img_up = upsamp(img_up, lvl)
 
-    img = img[:ico.vertices.shape[0]]
-    zeros_pad = np.zeros((nv_pad, 4))
-    img = np.concatenate((img, zeros_pad))
+    level += 1
+    for lvl in reversed(range(level, 8)):
+        img_down = downsamp(img, lvl)
 
-    #bilinear interpolation upsampling
-    img[ico_up.faces[3:][::4, 0]] = (img[ico.faces[:, 0]] + img[ico.faces[:, 1]]) / 2
-    img[ico_up.faces[3:][::4, 1]] = (img[ico.faces[:, 1]] + img[ico.faces[:, 2]]) / 2
-    img[ico_up.faces[3:][::4, 2]] = (img[ico.faces[:, 2]] + img[ico.faces[:, 1]]) / 2
+    out = img_up + img_down
+    out = normalize_features(out)
 
-    # plot the image and save it to disk
-    output_img_path = f"figs/area_1/{paths[0].split(os.path.sep)[-1].split('.')[0]}_upsample_test.html"
-    mp.plot(ico_up.vertices, ico_up.faces, c=img[:ico_up.vertices.shape[0], :3], shading={"point_size": 0.5}, filename=output_img_path)
-
-    output_img_path = f"figs/area_1/{paths[0].split(os.path.sep)[-1].split('.')[0]}_test.html"
-    mp.plot(ico.vertices, ico.faces, c=img[:ico.vertices.shape[0], :3], filename=output_img_path)
-
+    output_img_path = f"figs/op/add_out_{level - 1}.html"
+    ico_down = Icosphere(level=level - 1)
+    mp.plot(ico_down.vertices, ico_down.faces, c=np.mean(out, axis=-1),
+            shading={"point_size": 0.5, "colormap": "gray"}, filename=output_img_path)
--- uscnn/layers/layers.py
+++ uscnn/layers/layers.py
@@ -105,6 +105,55 @@ class MeshConv(_MeshConv):
         return out
 
 
+class MeshConvTest(_MeshConv):
+    def __init__(self, in_channels, out_channels, mesh_lvl, stride=1, bias=True):
+        # make a call to the parent class constructor
+        super(MeshConvTest, self).__init__(in_channels, out_channels, mesh_lvl, stride, bias)
+        pkl = self.pkl
+
+        if stride == 2:
+            self.nv_prev = pkl["nv_prev"]
+            L = sparse2tensor(pkl["L"].tocsr()[:self.nv_prev].tocoo())  # laplacian matrix V->V
+            F2V = sparse2tensor(pkl["F2V"].tocsr()[:self.nv_prev].tocoo())  # F->V, #V x #F
+        else:  # stride == 1
+            self.nv_prev = pkl["nv"]
+            L = sparse2tensor(pkl["L"].tocoo())
+            F2V = sparse2tensor(pkl["F2V"].tocoo())
+
+        self.register_buffer("L", L)
+        self.register_buffer("F2V", F2V)
+
+    def forward(self, input):
+        # identity
+        identity = input[..., :self.nv_prev]
+
+        # gradient
+        grad_face = spmatmul(input, self.G)
+        grad_face = grad_face.view(*(input.size()[:2]), 3, -1).permute(0, 1, 3, 2)  # gradient, 3 component per face
+
+        # laplacian
+        laplacian = spmatmul(input, self.L)
+
+        # face gradients along cardinal directions
+        grad_face_ew = torch.sum(torch.mul(grad_face, self.EW), keepdim=False, dim=-1)
+        grad_face_ns = torch.sum(torch.mul(grad_face, self.NS), keepdim=False, dim=-1)
+
+        # vertex gradients (weighted by face area)
+        grad_vert_ew = spmatmul(grad_face_ew, self.F2V)
+        grad_vert_ns = spmatmul(grad_face_ns, self.F2V)
+
+        # features
+        feat = [identity, laplacian, grad_vert_ew, grad_vert_ns]
+
+        # # dot product to compute the PDO convolution
+        # out = torch.stack(feat, dim=-1)
+        # out = torch.sum(torch.sum(torch.mul(out.unsqueeze(1), self.coeffs.unsqueeze(2)), dim=2), dim=-1)
+        # out += self.bias.unsqueeze(-1)
+
+        # return the computed feature maps
+        return torch.stack(feat, dim=0)
+
+
 class MeshConvTranspose(_MeshConv):
     def __init__(self, in_channels, out_channels, mesh_lvl, stride=2, bias=True):
         # assert for supported strides
